bd39b2ef896e0eaa452222623725e62ab4aa7c68
apuczka <a.puczka@icloud.com>
2023-02-14 12:03:58 +0100

    use Portal to Modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
+
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hidenCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartIsShown && <Cart />}
+      <Header onShownCart={showCartHandler} />
       <main>
-      <Meals />
+        <Meals />
       </main>
     </>
-
-
   );
 }
 
