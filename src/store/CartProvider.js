ae86a57114aa150b1716c1fbc99810cdf417f104
apuczka <a.puczka@icloud.com>
2023-02-15 11:38:54 +0100

    CartReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,10 +8,10 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if(action.type === 'ADD') {
       const updatedItems = state.items.concat(action.item);
-      const newTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+      const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
       return {
         items: updatedItems,
-        totalAmount: updatedItems
+        totalAmount: updatedTotalAmount
       };
   }
   return defaultCartState;
